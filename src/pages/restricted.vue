b5f113b146e164bb3120a6f48975a935a5d8e699
rifftyo <rifftyo@gmail.com>
2023-11-23 16:34:40 +0700

    Initial commit

---
+++ src/pages/restricted.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatibus quibusdam porro, natus tempore consectetur minima obcaecati fuga, earum tenetur, quisquam impedit a soluta corrupti asperiores repudiandae ea. Eum, cumque!.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum impedit possimus, similique quasi repellendus ab quae facere voluptates omnis nemo neque explicabo quam numquam, a repudiandae exercitationem consectetur distinctio.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut totam quisquam dignissimos obcaecati, quae, mollitia placeat odio rerum tempore quod repellat sequi dolorum eius fugit veritatis autem, nemo eaque? Eum?
+  </div>
+</template>
